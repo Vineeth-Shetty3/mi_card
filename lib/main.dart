8fcd14856b4a4ab3a27f10ca1414edb3aabf6aba
Vineeth Shetty <shettyvineeth1999@gmail.com>
2023-03-03 21:06:24 +0530

    my profile card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.teal.shade600,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/vineethr.jpg'),
+              ),
+              Text(
+                'Vineeth Shetty',
+                style: TextStyle(
+                    fontFamily: 'Mynerve',
+                    fontSize: 45.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    color: Colors.teal.shade50,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade50,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade800,
+                  ),
+                  title: Text(
+                    '+91 733 851 3644',
+                    style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade800,
+                  ),
+                  title: Text(
+                    'shettyvineeth1999@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
